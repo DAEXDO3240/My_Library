0a1252bcad49118b040928604217541bbaaf4212
DAEXDO3240 <42252180+DAEXDO3240@users.noreply.github.com>
2018-08-10 13:27:08 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+ 
+setup(name='My_Library',
+      version='0.1',
+      url='https://github.com/DAEXDO3240/My_Library',
+      license='MIT',
+      author='DAEXDO 3240',
+      author_email='david.proietto@gmail.com',
+      description='this library interacts in various fields, expanding and simplifying: it helps you manage tkinter files and math',
+      packages=find_packages(exclude=['tests']),
+      long_description=open('README.md').read(),
+      zip_safe=False)
