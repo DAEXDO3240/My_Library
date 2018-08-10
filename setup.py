7a166ff4df0f2f4f7fdae81610103d9bb417bba0
DAEXDO3240 <42252180+DAEXDO3240@users.noreply.github.com>
2018-08-10 13:55:20 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(name='My_Library',
       license='MIT',
       author='DAEXDO 3240',
       author_email='david.proietto@gmail.com',
-      description='this library interacts in various fields, expanding and simplifying: it helps you manage tkinter files and math',
+      description='this library interacts in various fields, expanding and simplifying, helps you manage the files: math and tkinter',
       packages=find_packages(exclude=['tests']),
       long_description=open('README.md').read(),
       zip_safe=False)
